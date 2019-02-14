c5051692f48a4695646c19db5349bf8863140282
Bogdan Ion <bogdan_ionut.ion@cti.pub.ro>
2019-02-14 21:49:17 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,18 @@
+package volumeprogram;
+
 import java.io.IOException;
 
 import javax.swing.JFrame;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		JFrame frame = new MenuDemo();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("Volume of an irregular truncated pyramid");
-		frame.setSize(1000, 600);
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
-	}
+    public static void main(String[] args) throws IOException {
+        JFrame frame = new MenuDemo();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Volume of an irregular truncated pyramid");
+        frame.setSize(1000, 600);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 
 }
